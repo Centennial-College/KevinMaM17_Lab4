cb72b916f5b0525139cedfdee260daa654722fe0
Kevin Ma <kbma.kevin@gmail.com>
2017-07-07 17:45:17 -0400

    created ListNode class

---
+++ Kevin-Ma_Lab04_Ex1/ListNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kevin_Ma_Lab04_Ex1
+{
+    public class ListNode
+    {
+        //PROPERTIES++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //auto implemented properties
+        //type of the data should be double
+        public double Data { get; set; }
+        //reference to next node in the LinkedList
+        public ListNode Next { get; set; }
+
+        //CONSTRUCTORS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //constructor to create ListNode that refers to dataValue and is last node in list
+        public ListNode(double dataValue) : this(dataValue, null) { }
+
+        //constructor to create ListNode that refers to dataValue and refers to the next ListNode in the List
+        public ListNode(double dataValue, ListNode nextNode)
+        {
+            Data = dataValue;
+            Next = nextNode;
+        }
+    }
+}
